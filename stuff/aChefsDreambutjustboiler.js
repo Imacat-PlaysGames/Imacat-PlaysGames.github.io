0841de4a1f7193c878b6ec03a40367c15953985a
Isaac <68208158+Imacat-PlaysGames@users.noreply.github.com>
2024-12-13 12:59:41 -0500

    Update aChefsDreambutjustboiler.js

--- stuff/aChefsDreambutjustboiler.js
+++ stuff/aChefsDreambutjustboiler.js
@@ -25,30 +25,4 @@ elements.boiler2 = {
 	noMix: true,
 	movable: false,
     temp: 150
-}
-elements.condenser = {
-	color: "#73fff8",
-	behavior: behaviors.WALL,
-	tick: function(pixel) {
-		for (var i = 0; i < squareCoords.length; i++) {
-                var coord = squareCoords[i];
-                var x = pixel.x+coord[0];
-                var y = pixel.y+coord[1];
-                if (!isEmpty(x,y, true)) {
-					if(pixelMap[x][y].temp < -230) {
-                    pixelMap[x][y].temp = (pixelMap[x][y].temp + 7)
-					} else if(pixelMap[x][y].temp > 270) {
-						pixelMap[x][y].temp = (pixelMap[x][y].temp - 7)
-					} else if (pixelMap[x][y].temp < 50) {
-						pixelMap[x][y].temp = (pixelMap[x][y].temp - 2
-					}
-                }
-            }
-	},
-	category:"machines",
-	state:"solid",
-	insulate: true,
-	noMix: true,
-	movable: false,
-    temp: 150
 };
